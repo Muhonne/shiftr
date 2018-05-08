a96ab94bee3c427b983244c33a59667b08ad9205
Mattias Muhonen <mattias.muhonen@soikea.com>
2018-05-08 15:58:54 +0300

    fix dumb date() in test

--- __tests__/components/ShiftListItem-test.js
+++ __tests__/components/ShiftListItem-test.js
@@ -2,6 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ShiftListItem from "../../src/components/ShiftListItem/View";
 
+const shiftDate = new Date(2000, 1, 1, 1, 1);
+
 describe("Button", () => {
   it("renders with minimun props", () => {
     expect(
@@ -9,8 +11,8 @@ describe("Button", () => {
         <ShiftListItem
           shift={{
             id: "test",
-            startTime: new Date(),
-            endTime: new Date(),
+            startTime: shiftDate,
+            endTime: shiftDate,
             booked: false
           }}
         />
